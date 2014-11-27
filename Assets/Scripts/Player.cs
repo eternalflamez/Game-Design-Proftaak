8b3f3ad545054fab042a202c3f28be5a4d958dd3
BlueHands <tomcyber4@hotmail.com>
2014-11-27 14:58:16 +0100

    Proof of Concept
    Muren toegevoegd, Level win toegevoegd.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,41 +6,70 @@ public class Player : MonoBehaviour {
     private float deceleration = 13;
     private float curSpeed = 0;
 
+	private bool isEnded = false;
+
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
-        this.transform.rotation = new Quaternion();
+		this.transform.rotation = new Quaternion();
 
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            curSpeed = -speed;
-        }
-        else if (Input.GetKey(KeyCode.RightArrow))
-        {
-            curSpeed = speed;
-        }
-        else
-        {
-            if (curSpeed > 0)
-            {
-                curSpeed -= deceleration * Time.deltaTime;
-            }
-            else if(curSpeed < 0)
-            {
-                curSpeed += deceleration * Time.deltaTime;
-            }
-        }
+		if (!isEnded)
+		{
+	        if (Input.GetKey(KeyCode.LeftArrow))
+	        {
+	            curSpeed = -speed;
+	        }
+	        else if (Input.GetKey(KeyCode.RightArrow))
+	        {
+	            curSpeed = speed;
+	        }
+	        else
+	        {
+	            if (curSpeed > 0)
+	            {
+	                curSpeed -= deceleration * Time.deltaTime;
+	            }
+	            else if(curSpeed < 0)
+	            {
+	                curSpeed += deceleration * Time.deltaTime;
+	            }
+	        }
 
-        if (Mathf.Abs(curSpeed) - .1f < 0)
-        {
-            curSpeed = 0;
-        }
+	        if (Mathf.Abs(curSpeed) - .1f < 0)
+	        {
+	            curSpeed = 0;
+	        }
 
-        this.rigidbody2D.velocity = new Vector2(curSpeed, this.rigidbody2D.velocity.y);
+	        this.rigidbody2D.velocity = new Vector2(curSpeed, this.rigidbody2D.velocity.y);
+		}
+	}
+
+	void OnGUI()
+	{
+		if (isEnded)
+		{
+			if (GUI.Button(new Rect(310,180,200,30), "Restart Level"))
+			{
+				Application.LoadLevel (0);
+			}
+		}
+	}
+
+	private void stopGame()
+	{
+		isEnded = true;
+
+		this.rigidbody2D.isKinematic = true;
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		stopGame ();
 	}
 }
