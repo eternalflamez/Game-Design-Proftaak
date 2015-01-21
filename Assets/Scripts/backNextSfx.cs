203fa1248992064098507c7ff06eb3ce01e2e2f7
Ustrel <kop_sander@hotmail.com>
2015-01-21 18:39:18 +0100

    Sound effecten toegevoegd/ Dice button
    Verder nog wat kleine veranderingen, oa de buttons veranderd.

---
+++ Assets/Scripts/backNextSfx.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+class backNextSfx : MonoBehaviour
+{
+	private static backNextSfx instance = null;
+	public static backNextSfx Instance
+	{
+		get
+		{
+			if (instance == null)
+			{
+				instance = (backNextSfx)FindObjectOfType(typeof(backNextSfx));
+			}
+			return instance;
+		}
+	}
+	
+	void Awake ()
+	{
+		if (Instance != this)
+		{
+			Destroy(gameObject);
+		}
+		else
+		{
+			DontDestroyOnLoad(gameObject);
+		}
+	}
+	
+	public void Destroy()
+	{
+		Destroy (gameObject);
+	}
+}
